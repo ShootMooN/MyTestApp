58ded3145aafb18c1209fdee000e512dac52d143
ShootMooN <shootmoon@outlook.com>
2015-06-23 21:43:05 +0800

    express first

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 
-var app = require('./app');
+var express = require('express');
+var app = express();
+
+app.use('/', function(req, res) {
+  res.send('Hello World');
+});
 
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
@@ -14,3 +19,5 @@ var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 var server = app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 });
+
+module.exports = app;
