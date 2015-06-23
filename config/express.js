004c9cc721a7c45c6f182302538df432b24d9bde
ShootMooN <shootmoon@outlook.com>
2015-06-23 22:32:35 +0800

    cloud

--- config/express.js
+++ config/express.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cloud = require('./cloud');
 
 module.exports = function(){
     var app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
+
+    // 加载云代码方法
+    app.use(cloud);
 
     require('../app/routes/index.server.routes.js')(app);
 
