4b1481e123361805bed95966f2fdf61b03651ec1
ShootMooN <shootmoon@outlook.com>
2015-06-24 21:21:05 +0800

    ejs

--- config/express.js
+++ config/express.js
@@ -3,15 +3,40 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cloud = require('./cloud');
+var methodOverride = require('method-override');
 
-module.exports = function(){
+module.exports = function () {
     var app = express();
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(methodOverride());
     app.use(cookieParser());
 
     // 加载云代码方法
     app.use(cloud);
+
+    if (app.get('env') === 'development') {
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    } else {
+        app.use(compress());
+
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: {}
+            });
+        });
+    }
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
 
     require('../app/routes/index.server.routes.js')(app);
 
