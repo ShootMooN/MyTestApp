81891fe405ad01d6a610f22ff57ddd3167350ab8
ShootMooN <shootmoon@outlook.com>
2015-06-24 21:50:19 +0800

    555

--- config/express.js
+++ config/express.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cloud = require('./cloud');
@@ -7,15 +6,31 @@ var methodOverride = require('method-override');
 
 module.exports = function () {
     var app = express();
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(cookieParser());
 
+    if(app.get('env') === 'production'){
+        app.use(compress());
+    }
+
     // 加载云代码方法
     app.use(cloud);
 
-   /* if (app.get('env') === 'development') {
+    require('../app/routes/index.server.routes.js')(app);
+
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    if (app.get('env') === 'development') {
         app.use(function (err, req, res, next) {
             res.status(err.status || 500);
             res.render('error', {
@@ -24,8 +39,6 @@ module.exports = function () {
             });
         });
     } else {
-        app.use(compress());
-
         app.use(function (err, req, res, next) {
             res.status(err.status || 500);
             res.render('error', {
@@ -33,12 +46,7 @@ module.exports = function () {
                 error: {}
             });
         });
-    }*/
-
-    app.set('views', './app/views');
-    app.set('view engine', 'ejs');
-
-    require('../app/routes/index.server.routes.js')(app);
+    }
 
     return app;
 }
