56e56b2f0f76e2e293ceaad0d5d536f452fe7efd
ShootMooN <shootmoon@outlook.com>
2015-06-24 21:41:25 +0800

    444

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ var methodOverride = require('method-override');
 
 module.exports = function () {
     var app = express();
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(cookieParser());
@@ -35,8 +35,8 @@ module.exports = function () {
         });
     }*/
 
-    //app.set('views', './app/views');
-    //app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
 
     require('../app/routes/index.server.routes.js')(app);
 
