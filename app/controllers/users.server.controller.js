78ac7dd3f486da9a3bedab6aa51cf9860531cc4f
ShootMooN <shootmoon@outlook.com>
2015-07-31 21:42:59 +0800

    create user 2

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,18 @@
+﻿var AV = require('leanengine');
+
+exports.create = function (req, res, next) {
+    var user = new AV.User();
+    user.set("username", req.body.username);
+    user.set("password", req.body.password);
+    user.set("email", req.body.email);
+    user.set("phone", req.body.phone);
+
+    user.signUp(null, {
+        success: function (user) {
+            res.json(user);
+        },
+        error: function (user, error) {
+            return next(err);
+        }
+    });
+};
---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,5 @@
+﻿var users = require('../controllers/users.server.controller');
+
+module.exports = function (app) {
+    app.route('/users').post(users.create);
+};
