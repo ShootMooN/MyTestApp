e6eaad83cf3caa2e4fe3e2865bfcc07eba18def9
ShootMooN <shootmoon@outlook.com>
2015-08-08 14:52:58 +0800

    angular service

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -2,7 +2,8 @@ exports.render = function (req, res) {
     if (req.user) {
         res.render("index", {
             title: "Hello World",
-            userFullName: req.user.get("nickname") || req.user.get("username")
+            user: JSON.stringify({"name": req.user.get("nickname") || req.user.get("username")})
+            //userFullName: req.user.get("nickname") || req.user.get("username")
         });
     }else {
         return res.redirect("/oauth/wechat");
